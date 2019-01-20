655c3c9e646894d48af4c20e68c22f9d51ab8fdf
Pelle Braendgaard <pelle@stakeventures.com>
2019-01-20 00:10:35 -0600

    build es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  entry: './lib/register.js',
+  entry: './src/register.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'nacl-did.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
   }
 }
